6f5b30cc01abe89c32edc1dc6b592c648db11d74
Igor Syrotenko <Neloreck@gmail.com>
2019-02-18 17:32:35 +0200

    Removed odd logger instance.

--- src/main/application/Application.ts
+++ src/main/application/Application.ts
@@ -11,14 +11,13 @@ import { EntryPoint } from "@Lib/utils/EntryPoint";
 export class Application {
 
   private static readonly PORT: number = Number.parseInt(process.env.PORT || "3000", 10);
-  private static readonly LOGGER: Logger = new Logger(Application.name);
 
   public static async main(): Promise<void> {
 
     const app = await NestFactory.create(ApplicationModule);
     await app.listen(this.PORT);
 
-    this.LOGGER.log(`Listening on port: ${this.PORT}.`);
+    Logger.log(`Listening on port: ${this.PORT}.`, Application.name);
   }
 
 }
